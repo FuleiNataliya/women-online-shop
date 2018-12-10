fcb4cedcf7cedf5a2f3a28798e171e994e09ac91
Zhenyya <jane07oks@gmail.com>
2018-12-10 22:40:58 +0200

    Added ProductMapper, ProductDtoWithId, changed ProductController

--- src/main/java/com/women/online/shop/controllers/ProductController.java
+++ src/main/java/com/women/online/shop/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.women.online.shop.controllers;
 
 import com.women.online.shop.controllers.dto.ProductDto;
+import com.women.online.shop.controllers.dto.ProductDtoWithId;
+import com.women.online.shop.controllers.mappers.ProductMapper;
 import com.women.online.shop.entities.Product;
 import com.women.online.shop.repositoties.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,30 +23,31 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public ResponseEntity create1(@RequestBody ProductDto productDto) {
-        final Product product = new Product(productDto.getName(), productDto.getPrice(),
-                productDto.getCountry());
+    public ResponseEntity create(@RequestBody ProductDto productDto) {
+        final Product product = ProductMapper.toProduct(productDto);
         productRepository.save(product);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @GetMapping("/products")
-    public ResponseEntity getProducts() {
-        final List<Product> all = productRepository.findAll();
-        if (!all.isEmpty()) {
-            return ResponseEntity.ok(all);
-        } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+    public ResponseEntity<List<ProductDtoWithId>> getProducts() {
+        final List<Product> allProducts = productRepository.findAll();
+        List<ProductDtoWithId> all = new ArrayList<>();
+        for (Product product : allProducts) {
+            ProductDtoWithId productDto = ProductMapper.toProductDto(product);
+            all.add(productDto);
         }
+        return ResponseEntity.ok(all);
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity findById(@PathVariable String id) {
+    public ResponseEntity<ProductDtoWithId> findById(@PathVariable String id) {
         final Optional<Product> product = productRepository.findById(id);
         if (product.isPresent()) {
-            return ResponseEntity.ok(product);
+            ProductDtoWithId productDto = ProductMapper.toProductDto(product.get());
+            return ResponseEntity.ok(productDto);
         } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
     }
 
@@ -54,7 +58,7 @@ public class ProductController {
             productRepository.deleteById(id);
             return new ResponseEntity(HttpStatus.OK);
         } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
     }
 }
