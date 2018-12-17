77448111b7ecd72acd5a284a7d3e4bfc578eb5c0
Ronashovkova <rona_t@icloud.com>
2018-12-17 19:50:34 +0200

    FindProductByCategory

--- src/main/java/com/women/online/shop/repositoties/ProductRepository.java
+++ src/main/java/com/women/online/shop/repositoties/ProductRepository.java
@@ -3,5 +3,8 @@ package com.women.online.shop.repositoties;
 import com.women.online.shop.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, String> {}{
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+    Product findByCategories(String shortName);
 }
+
